8efbc10c79987d5f7d729ac58e42fa43af1240bb
JohnK94 <keeganjohn11@yahoo.com>
2017-12-02 15:43:10 +0000

    s00149671part4

---
+++ Rad301ClubsV1/Controllers/ClubMemberController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rad301ClubsV1.Controllers
+{
+    public class ClubMemberController : Controller
+    {
+        // GET: ClubMember
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ClubMember/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClubMember/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClubMember/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClubMember/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClubMember/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClubMember/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClubMember/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
